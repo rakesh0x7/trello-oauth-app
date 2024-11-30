f0883f82bd061ded071e619b1e7880e522fc6fa2
rakesh0x7 <19131a05p3@gvpce.ac.in>
2024-11-30 06:54:20 +0530

    default settings

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
-const appnameRoutes = require("./routes/appname");
+const trelloRoutes = require("./routes/trello");
 
 // Routes
 app.use('/api/auth', require('./routes/auth'));
-app.use("/", appnameRoutes);
+app.use("/", trelloRoutes);
 
 // Start Server
 app.listen(PORT, () => {
